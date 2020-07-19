94de952be6bbc50958d6ac5cafde90db01ac1699
Billie Thompson <billie@billiecodes.com>
2020-07-19 19:08:28 +0200

    feat!: Add support for weather pattern
    This adds environmental data to the logged data. This is pretty
    important to when lights come on and go off.
    
    BREAKING CHANGE: The CSV is in a different format and all the options
    have changed

--- README.md
+++ README.md
@@ -7,19 +7,23 @@ training neural networks and similar.
 ## Usage
 
 ``` shell
-log-light-state --help
-log-light-state 0.1.0
-billie
-
+log-light-state 0.1.2
+Billie Thompson <billie+git-mit@billiecodes.com>
+log-light-state is a tool to generate a CSV of all your hue light states every minuit. The intention is that you'd use
+that log for training neural networks and similar.
 
 USAGE:
-    log-light-state <hue-user> [output]
+    log-light-state [OPTIONS] --hue-user <hue-user> --lat <latitude> --lon <longitude> --openweather-api-key <openweather-api-key>
 
 FLAGS:
     -h, --help       Prints help information
     -V, --version    Prints version information
 
-ARGS:
-    <hue-user>    Username to use on the hue hub to interact with the API
-    <output>      Where to write the CSV to. Put - for stdout [default: /dev/stdout]
+OPTIONS:
+    -u, --hue-user <hue-user>                          Username to use on the hue hub to interact with the API
+    -a, --lat <latitude>                               The latitude where the readings are being taken from
+    -l, --lon <longitude>                              The longitude where the readings are being taken from
+    -w, --openweather-api-key <openweather-api-key>    API key to use to interact with the OpenWeather API
+    -o, --output <output>                              Where to write the CSV to [default: /dev/stdout]
+    -p, --poll-every-seconds <poll>                    Poll every seconds [default: 60]
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::{thread, time};
+
+use serde::Deserialize;
+use serde::Serialize;
 use std::error::Error;
 use std::fs::{File, OpenOptions};
 use std::path::Path;
-use std::{thread, time};
+use std::str::FromStr;
 
 use chrono::prelude::*;
 use clap::crate_authors;
@@ -12,6 +16,93 @@ use clap::{App, Arg};
 use csv::Writer;
 use hueclient::bridge::Bridge;
 use indicatif::{ProgressBar, ProgressStyle};
+use spa::{calc_sunrise_and_set, SunriseAndSet};
+
+#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
+struct WeatherClouds {
+    all: i64,
+}
+
+#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
+struct WeatherMain {
+    feels_like: f64,
+    humidity: f64,
+    pressure: f64,
+    temp: f64,
+    temp_max: f64,
+    temp_min: f64,
+}
+
+#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
+struct WeatherDescription {
+    id: i64,
+}
+
+#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
+struct WeatherWind {
+    deg: i64,
+    speed: f64,
+}
+
+#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
+struct WeatherRain {
+    #[serde(rename(serialize = "1h"))]
+    one_h: i64,
+    #[serde(rename(serialize = "3h"))]
+    three_h: i64,
+}
+
+#[derive(Debug, Deserialize, Serialize, Copy, Clone)]
+struct WeatherSnow {
+    #[serde(rename(serialize = "1h"))]
+    one_h: i64,
+    #[serde(rename(serialize = "3h"))]
+    three_h: i64,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct WeatherResponse {
+    main: WeatherMain,
+    visibility: i64,
+    weather: Vec<WeatherDescription>,
+    clouds: Option<WeatherClouds>,
+    wind: Option<WeatherWind>,
+    rain: Option<WeatherRain>,
+    snow: Option<WeatherSnow>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct OutputRow {
+    timestamp: String,
+    weather_main_feels_like: f64,
+    weather_main_humidity: f64,
+    weather_main_pressure: f64,
+    weather_main_temp: f64,
+    weather_main_temp_max: f64,
+    weather_main_temp_min: f64,
+    weather_visibility: i64,
+    weather_weather_0_id: Option<i64>,
+    weather_clouds_all: Option<i64>,
+    weather_wind_deg: Option<i64>,
+    weather_wind_speed: Option<f64>,
+    weather_rain_1h: Option<i64>,
+    weather_rain_3h: Option<i64>,
+    weather_snow_1h: Option<i64>,
+    weather_snow_3h: Option<i64>,
+    light_name: String,
+    light_modelid: String,
+    light_swversion: String,
+    light_uniqueid: String,
+    light_state_on: bool,
+    light_state_bri: u8,
+    light_state_hue: u16,
+    light_state_sat: u8,
+    light_state_ct: Option<u16>,
+    light_state_xy_0: Option<f32>,
+    light_state_xy_1: Option<f32>,
+    id: usize,
+    daylight: bool,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new(crate_name!())
@@ -21,21 +112,63 @@ fn main() -> Result<(), Box<dyn Error>> {
         .about(crate_description!())
         .arg(
             Arg::with_name("hue-user")
+                .short("u")
+                .long("hue-user")
                 .required(true)
+                .takes_value(true)
                 .help("Username to use on the hue hub to interact with the API"),
         )
         .arg(
+            Arg::with_name("openweather-api-key")
+                .short("w")
+                .long("openweather-api-key")
+                .required(true)
+                .takes_value(true)
+                .help("API key to use to interact with the OpenWeather API"),
+        )
+        .arg(
+            Arg::with_name("longitude")
+                .short("l")
+                .long("lon")
+                .takes_value(true)
+                .required(true)
+                .help("The longitude where the readings are being taken from"),
+        )
+        .arg(
+            Arg::with_name("poll")
+                .short("p")
+                .long("poll-every-seconds")
+                .takes_value(true)
+                .default_value("60")
+                .help("Poll every seconds"),
+        )
+        .arg(
+            Arg::with_name("latitude")
+                .short("a")
+                .long("lat")
+                .takes_value(true)
+                .required(true)
+                .help("The latitude where the readings are being taken from"),
+        )
+        .arg(
             Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .takes_value(true)
                 .required(false)
                 .help("Where to write the CSV to")
                 .default_value("/dev/stdout"),
         )
         .get_matches();
 
+    let openweather_api = matches.value_of("openweather-api-key").unwrap();
     let hue_user = matches.value_of("hue-user").unwrap();
     let output = matches.value_of("output").unwrap_or("/dev/stdout");
-    let is_stdout = output == "/dev/stdout";
+    let lat = f64::from_str(matches.value_of("latitude").unwrap())?;
+    let lon = f64::from_str(matches.value_of("longitude").unwrap())?;
+    let poll_seconds = i64::from_str(matches.value_of("poll").unwrap())?;
 
+    let is_stdout = output == "/dev/stdout";
     let path = Path::new(output);
     let file = OpenOptions::new()
         .write(true)
@@ -43,30 +176,57 @@ fn main() -> Result<(), Box<dyn Error>> {
         .append(true)
         .open(path)?;
 
-    let mut csv_writer = csv::Writer::from_writer(file);
+    let mut csv_writer = csv::WriterBuilder::new()
+        .has_headers(is_stdout || !path.is_file() || path.metadata()?.len() == 0)
+        .from_writer(file);
+
     let bridge = Bridge::discover().unwrap().with_user(hue_user.into());
 
-    let headers = &[
-        "timestamp",
-        "id",
-        "light.name",
-        "light.modelid",
-        "light.swversion",
-        "light.uniqueid",
-        "light.state.on",
-        "light.state.bri",
-        "light.state.hue",
-        "light.state.sat",
-        "light.state.ct",
-        "light.state.xy.0",
-        "light.state.xy.1",
-    ];
+    poll(
+        openweather_api,
+        lat,
+        lon,
+        is_stdout,
+        &mut csv_writer,
+        &bridge,
+        poll_seconds,
+    )?;
 
-    if is_stdout || !path.is_file() || path.metadata()?.len() == 0 {
-        csv_writer.write_record(headers)?;
-        csv_writer.flush()?;
+    Ok(())
+}
+
+fn poll(
+    openweather_api: &str,
+    lat: f64,
+    lon: f64,
+    is_stdout: bool,
+    mut csv_writer: &mut Writer<File>,
+    bridge: &Bridge,
+    poll_every_seconds: i64,
+) -> Result<(), Box<dyn Error>> {
+    let progress = progress_bar(is_stdout);
+
+    for i in 0.. {
+        if i % poll_every_seconds == 0 {
+            output_lights(
+                &mut csv_writer,
+                &bridge,
+                &progress,
+                lat,
+                lon,
+                openweather_api,
+            )?;
+        }
+
+        progress.set_message(&format!("Sleeping for {} sec", poll_every_seconds));
+        progress.tick();
+        thread::sleep(time::Duration::from_secs(1));
     }
 
+    Ok(())
+}
+
+fn progress_bar(is_stdout: bool) -> ProgressBar {
     let progress = if is_stdout {
         ProgressBar::hidden()
     } else {
@@ -76,27 +236,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     progress.set_style(ProgressStyle::default_bar().template("{spinner} {elapsed_precise} {msg}"));
     progress.set_message("Starting event loop");
     progress.tick();
-
-    for i in 0.. {
-        if i % 60 == 0 {
-            output_lights(&mut csv_writer, &bridge, &progress)?;
-        }
-
-        progress.set_message(&format!("Sleeping for {} sec", 60));
-        progress.tick();
-        thread::sleep(time::Duration::from_secs(1));
-    }
-
-    Ok(())
+    progress
 }
 
 fn output_lights(
     file_writer: &mut Writer<File>,
     bridge: &Bridge,
     progress: &ProgressBar,
+    lat: f64,
+    lon: f64,
+    openweather_api_key: &str,
 ) -> Result<(), Box<dyn Error>> {
     progress.set_message("Polling hub");
     progress.tick();
+
+    let daylight = match calc_sunrise_and_set(Utc::now(), lat, lon)? {
+        SunriseAndSet::PolarNight => false,
+        SunriseAndSet::PolarDay => true,
+        SunriseAndSet::Daylight(rise, set) => rise <= Utc::now() && Utc::now() <= set,
+    };
+
+    let weather: WeatherResponse = reqwest::blocking::get(&format!(
+        "https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}",
+        lat, lon, openweather_api_key
+    ))?
+    .json()?;
 
     for light in &bridge.get_all_lights().unwrap() {
         progress.set_message(&format!(
@@ -105,35 +269,37 @@ fn output_lights(
         ));
         progress.tick();
 
-        let row = &[
-            &Local::now().to_rfc3339(),
-            &format!("{}", light.id),
-            &light.light.name,
-            &light.light.modelid,
-            &light.light.swversion,
-            &light.light.uniqueid,
-            &light.light.state.on.to_string(),
-            &light.light.state.bri.to_string(),
-            &light.light.state.hue.to_string(),
-            &light.light.state.sat.to_string(),
-            &light
-                .light
-                .state
-                .ct
-                .map_or_else(|| "NaN".into(), |z| z.to_string()),
-            &light
-                .light
-                .state
-                .xy
-                .map_or_else(|| "NaN".into(), |x| x.0.to_string()),
-            &light
-                .light
-                .state
-                .xy
-                .map_or_else(|| "NaN".into(), |x| x.1.to_string()),
-        ];
-
-        file_writer.write_record(row)?;
+        file_writer.serialize(OutputRow {
+            timestamp: Local::now().to_rfc3339(),
+            weather_main_feels_like: weather.main.feels_like,
+            weather_main_humidity: weather.main.humidity,
+            weather_main_pressure: weather.main.pressure,
+            weather_main_temp: weather.main.temp,
+            weather_main_temp_max: weather.main.temp_max,
+            weather_main_temp_min: weather.main.temp_min,
+            weather_visibility: weather.visibility,
+            weather_weather_0_id: weather.weather.get(0).map(|x| x.id),
+            weather_clouds_all: weather.clouds.map(|x| x.all),
+            weather_wind_deg: weather.wind.map(|x| x.deg),
+            weather_wind_speed: weather.wind.map(|x| x.speed),
+            weather_rain_1h: weather.rain.map(|x| x.one_h),
+            weather_rain_3h: weather.rain.map(|x| x.three_h),
+            weather_snow_1h: weather.snow.map(|x| x.one_h),
+            weather_snow_3h: weather.snow.map(|x| x.three_h),
+            light_name: light.light.name.clone(),
+            light_modelid: light.light.modelid.clone(),
+            light_swversion: light.light.swversion.clone(),
+            light_uniqueid: light.light.uniqueid.clone(),
+            light_state_on: light.light.state.on,
+            light_state_bri: light.light.state.bri,
+            light_state_hue: light.light.state.hue,
+            light_state_sat: light.light.state.sat,
+            light_state_ct: light.light.state.ct,
+            light_state_xy_0: light.light.state.xy.map(|x| x.0),
+            light_state_xy_1: light.light.state.xy.map(|x| x.1),
+            id: 0,
+            daylight,
+        })?;
         file_writer.flush()?;
     }
 
